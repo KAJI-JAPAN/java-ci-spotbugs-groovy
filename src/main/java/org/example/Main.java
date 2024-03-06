07caf617cbaaf6539f38c5261e20af5d25f03f4a
KAJI-JAPAN <kurowassan1206@gmail.com>
2024-03-06 11:58:56 +0900

    SpotBugs push test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+  static  Long a = 129L;
+  static Long b = 129L;
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        if(a == b) {
+            System.out.println("＝＝での比較");
+        }
+        System.out.println("Hello world!!!"
+        );
     }
 }
